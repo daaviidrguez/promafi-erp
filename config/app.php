a2baab1b342f407f6b13877e532405ab8520d26c
David Elias Rodriguez de la Cruz <david@MacBook-Air-de-David.local>
2026-02-20 20:34:48 -0600

    fecha villahermosa

--- config/app.php
+++ config/app.php
@@ -60,12 +60,12 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you may specify the default timezone for your application, which
-    | will be used by the PHP date and date-time functions. The timezone
-    | is set to "UTC" by default as it is suitable for most use cases.
+    | will be used by the PHP date and date-time functions. Villahermosa,
+    | Tabasco, México usa la zona horaria America/Mexico_City (Centro).
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => env('APP_TIMEZONE', 'America/Mexico_City'),
 
     /*
     |--------------------------------------------------------------------------
